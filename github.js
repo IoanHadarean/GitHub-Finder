a642d50a2551769a5019c41acb3202fb198017bb
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-05-06 06:43:38 +0000

    Added class for fetching the user profile with async/await/added clientId and clientSecret for more than 100 requests per hour/not advisable in production

---
+++ github.js
@@ -0,0 +1,20 @@
+/* global fetch */
+
+
+class GitHub {
+    constructor() {
+        this.clientId = 'fef70f7521da2c57d51f';
+        this.clientSecret = 'a32d691ec9953d3225d42371e84112bf4816f168';
+    }
+    
+    async getUser(user) {
+        const profileResponse =  await fetch(`https://api.github.com/users/${user}?client_id=${this.clientId}&client_secret=${this.clientSecret}`);
+        const profile =  await profileResponse.json();
+        
+        return {
+            profile
+        }
+        
+        
+    }
+}
